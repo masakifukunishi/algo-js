574421d775f042b9df8b06daef43aa52c441fae3
MasakiFukunishi <fmsknn@gmail.com>
2022-11-07 17:09:55 -0800

    Add binary search

--- search/binary_search.js
+++ search/binary_search.js
@@ -1,17 +1,33 @@
+// const binarySearch = (numbers, value) => {
+//   let [left, right] = [0, numbers.length - 1];
+//   while (left <= right) {
+//     const mid = Math.floor((left + right) / 2);
+//     if (numbers[mid] === value) {
+//       return mid;
+//     } else if (numbers[mid] < value) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+//   return -1;
+// };
+
 const binarySearch = (numbers, value) => {
-  let [left, right] = [0, numbers.length - 1];
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+  const _binarySearch = (numbers, value, left, right) => {
+    if (left > right) {
+      return -1;
+    }
+    const mid = Math.floor((left + right) / 2);
     if (numbers[mid] === value) {
       return mid;
     } else if (numbers[mid] < value) {
-      left = mid + 1;
+      return _binarySearch(numbers, value, mid + 1, right);
     } else {
-      right = mid - 1;
+      return _binarySearch(numbers, value, left, mid - 1);
     }
-  }
-  return -1;
+  };
+  return _binarySearch(numbers, value, 0, numbers.length - 1);
 };
-
 const nums = [1, 2, 5, 6, 7, 9];
-console.log(binarySearch(nums, 1));
+console.log(binarySearch(nums, 7));
