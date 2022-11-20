73b8c4be45fbdfee9b210a1afb05a4fa0f463a37
MasakiFukunishi <fmsknn@gmail.com>
2022-11-20 07:28:16 -0800

    Add linked list insert

--- dataStructures/linkedList.js
+++ dataStructures/linkedList.js
@@ -28,9 +28,44 @@ class LinkedList {
     this.length++;
     return this;
   }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-myLinkedList.append(15);
-myLinkedList.prepend(16);
-console.log(myLinkedList);
+myLinkedList.append(5);
+// myLinkedList.append(16);
+// myLinkedList.prepend(1);
+// myLinkedList.insert(2, 99);
+// console.log(myLinkedList.printList());
+// console.log(myLinkedList);
