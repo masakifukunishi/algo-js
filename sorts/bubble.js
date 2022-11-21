94b680845d24f45573b4f4fd8bf24983e440d930
MasakiFukunishi <fmsknn@gmail.com>
2022-11-21 15:35:24 -0800

    Add bubble sort

--- sorts/bubble.js
+++ sorts/bubble.js
@@ -1,13 +1,15 @@
-const bubbleSort = (numbers) => {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length - 1 - i; j++) {
-      if (numbers[j] > numbers[j + 1]) {
-        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
-  return numbers;
-};
+}
 
-const nums = [...Array(20).keys()].map(() => Math.floor(Math.random() * 100));
-console.log(bubbleSort(nums));
+bubbleSort(numbers);
+console.log(numbers);
